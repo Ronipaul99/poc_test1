1b22d8fba219d60c2a0e03664bc88fb23b1371aa
Reshab <naskarreshab2003@gmail.com>
2023-01-25 18:51:26 +0530

    Third commit

---
+++ src/Components/Layout/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import StudentDashboard from "../Pages/S-Dashboard";
+import { U } from "../Store/User";
+
+
+export default function Dashboard() {
+    const e = useSelector(U);
+    return (
+        <>
+            {e==="S" && <StudentDashboard />}
+        </>
+    );
+};
