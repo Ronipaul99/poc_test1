232851e053350fdf609ce562131628f476751d4a
Reshab <naskarreshab2003@gmail.com>
2023-01-25 19:10:10 +0530

    Fourth commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Auth from "./Components/Auth";
+import Auth from "./Components/Auth/Auth";
 import Dashboard from "./Components/Layout/Dashboard";
 import { U } from "./Components/Store/User";
 
