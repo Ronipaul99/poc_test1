ade04415f7276c470cdaa302584ce32fdfaf1785
Reshab <naskarreshab2003@gmail.com>
2023-01-30 14:49:21 +0530

    some changes

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -8,65 +8,66 @@ import AccordionDetails from '@mui/material/AccordionDetails';
 
 function Info(props) {
     return (
-        <Accordion
-            sx={{
-                width: "84.5vw",
-                backgroundColor: "#fff",
-                height: "80%",
-                padding: "20px",
-                justifyContent: "center"
-            }} >
-            <AccordionSummary
-                // expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
-            >
-                <Box sx={{
-                    minHeight: "23vh",
-                    display: "flex",
+        <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Accordion
+                sx={{
+                    width: "77.5vw",
+                    backgroundColor: "#fff",
+                    height: "80%",
+                    padding: "20px",
                     justifyContent: "center"
-                }}>
-                    <Card sx={{
-                        height: "22vh",
-                        width: "84.5vw",
-                        backgroundColor: "#fff",
-                        position: "relative",
-                        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+                }} >
+                <AccordionSummary
+                    // expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Box sx={{
+                        minHeight: "23vh",
+                        display: "flex",
+                        justifyContent: "center"
                     }}>
-                        <Box sx={{
-                            width: "100%",
-                            height: "100%",
-                            display: "flex",
+                        <Card sx={{
+                            height: "22vh",
+                            width: "78vw",
+                            backgroundColor: "#fff",
+                            position: "relative",
+                            boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
                         }}>
-
                             <Box sx={{
-                                width: "30%",
-                                backgroundColor: "#fff",
+                                width: "100%",
                                 height: "100%",
-                                overflow: "hidden"
+                                display: "flex",
                             }}>
-                                <img style={{
-                                    boxSizing: "border-box",
-                                    objectFit: "cover",
+
+                                <Box sx={{
+                                    width: "30%",
+                                    backgroundColor: "#fff",
                                     height: "100%",
-                                    width: "100%"
-                                }} src={props.imgurl} alt="pic here" />
+                                    overflow: "hidden"
+                                }}>
+                                    <img style={{
+                                        boxSizing: "border-box",
+                                        objectFit: "cover",
+                                        height: "100%",
+                                        width: "100%"
+                                    }} src={props.imgurl} alt="pic here" />
+                                </Box>
+                                {/* product heading details */}
                             </Box>
-                            {/* product heading details */}
-                        </Box>
-                    </Card>
-                </Box>
-            </AccordionSummary>
-            <AccordionDetails>
-                <Typography>
-                    <Typography> {props.name} </Typography>
+                        </Card>
+                    </Box>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        <Typography> {props.name} </Typography>
 
-                    <Typography> {props.rollNo}</Typography>
-                    {props.details}
-                </Typography>
-            </AccordionDetails>
-        </Accordion>
-
+                        <Typography> {props.rollNo}</Typography>
+                        {props.details}
+                    </Typography>
+                </AccordionDetails>
+            </Accordion>
+        </Box>
     );
 };
 export default Info;
