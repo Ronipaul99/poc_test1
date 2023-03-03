4c50c4a447606cd262416705a6a7c6491aa9e600
Reshab <naskarreshab2003@gmail.com>
2023-03-03 16:09:56 +0530

    publish

--- src/Components/Pages/T-Dashboard/T-Dashboard.js
+++ src/Components/Pages/T-Dashboard/T-Dashboard.js
@@ -1,10 +1,11 @@
+import { Box, Typography } from '@mui/material'
 import React from 'react'
 
 const TDashboard = () => {
   return (
-    <div>
-      
-    </div>
+    <Box>
+<Typography>xcfgvdxfcgxdfg</Typography>
+    </Box>
   )
 }
 
