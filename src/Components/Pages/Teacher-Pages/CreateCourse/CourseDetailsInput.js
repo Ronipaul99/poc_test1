1723d2181baad4fa408519ec7e9021f2dc043142
Reshab <naskarreshab2003@gmail.com>
2023-03-13 16:09:11 +0530

    bv

--- src/Components/Pages/Teacher-Pages/CreateCourse/CourseDetailsInput.js
+++ src/Components/Pages/Teacher-Pages/CreateCourse/CourseDetailsInput.js
@@ -11,95 +11,118 @@ import InputAdornment from '@mui/material/InputAdornment';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
-import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
-import PinOutlinedIcon from '@mui/icons-material/PinOutlined';
 import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import kp from "../../../Images/kp.png"
+import { useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 
 const CourseDetailsInput = () => {
 
     const [value, setValue] = React.useState(dayjs('2023-03-20'));
+    const [Number, setNumber] = React.useState([1]);
+
     const [checked, setChecked] = React.useState(false);
 
     const handleChange = (event) => {
         setChecked(event.target.checked);
     };
 
+    var arr = [];
+
+    for (var i = 1; i <= parseFloat(Number); i++) {
+        arr.push(i)
+    };
+    const handleChange1 = event => {
+        setNumber(event.target.value);
+    };
+
+
     const today = dayjs();
     const todayStartOfTheDay = today.startOf('day');
 
+
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1);
+    }
 
     return (
         <>
             <NavDrawer />
             <TDrawer />
             <Box sx={styles.mainContainer}>
-                <Card sx={styles.Card2}>
-                    <Box sx={styles.Box1}>
-                        <Typography sx={styles.Text1}>Fill the reqired data to cteate a course</Typography>
+                <Box sx={styles.Box2}>
+                    <ArrowBackIcon fontSize='medium' onClick={goBack} />
+                    <Box sx={styles.Boximg}>
+                        <Box sx={styles.Box50L}>
+                            <img style={{
+                                boxSizing: "border-box",
+                                objectFit: "cover",
+                                overflow: "hidden",
+                                height: "100%",
+                                width: "100%"
+                            }} src={kp} alt="pic here" />
+                        </Box>
+                    </Box>
+                    <Card sx={styles.Box50R}>
+                        <Typography sx={styles.Text1}>Cteate course</Typography>
                         <Box
                             component="form"
                             sx={{
-                                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                                display: "flex",
+                                flexDirection: "column",
+                                width: "100%",
+                                '& .MuiTextField-root': { m: 2 },
                             }}
                             noValidate
                             autoComplete="off"
                         >
-                            <TextField id="NumberOfCourse" label="Number of course" InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        < PinOutlinedIcon />
-                                    </InputAdornment>
-                                ),
-                            }} variant="standard" />
-                            <TextField id="CourseName" label="Course name" InputProps={{
-                                startAdornment: (
-                                    <InputAdornment position="start">
-                                        <CreateOutlinedIcon />
-                                    </InputAdornment>
-                                ),
-                            }} variant="standard" />
-                            <TextField id="CourseDetails" label="Course details" multiline maxRows={4}
-                                InputProps={{
-                                    startAdornment: (
-                                        <InputAdornment position="start">
-                                            <InfoOutlinedIcon />
-                                        </InputAdornment>
-                                    ),
-                                }} variant="standard" />
-                            <Typography sx={styles.Text2}>Schedule</Typography>
-                            <br />
-                            <LocalizationProvider dateAdapter={AdapterDayjs}>
-                                <DatePicker
-                                    label="Choose date"
-                                    value={value}
-                                    onChange={(newValue) => setValue(newValue)}
-                                />
+                            <TextField id="Title" label="Title" size="small" />
+                            <Box sx={styles.Textfield1}>
+                                <TextField id="NumberOfClasses" label="Number of classes" onChange={handleChange1} value={Number} sx={{ width: '300px' }} size="small" />
+                                <TextField id="StartDate" label="Start date" sx={{ width: '300px' }} size="small" />
+                            </Box>
+                            <Box sx={styles.Textfield1}>
+                                <TextField id="Subject" label="Subject" sx={{ width: '300px' }} size="small" />
+                                <TextField id="SubjectTopic" label="Subject topic" sx={{ width: '300px' }} size="small" />
+                            </Box>
+                            {arr.map((user) => (
+                                <Box key={user} sx={{ display: "flex", width: "100%", }}>
+                                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+                                        <DatePicker
+                                            label="Choose date"
+                                            value={value}
+                                            onChange={(newValue) => setValue(newValue)}
+                                        />
 
-                                <TimePicker label="Start from" defaultValue={todayStartOfTheDay} />
+                                        <TimePicker label="Start from" defaultValue={todayStartOfTheDay} />
 
 
-                                <TimePicker label="End at" defaultValue={todayStartOfTheDay} />
+                                        <TimePicker label="End at" defaultValue={todayStartOfTheDay} />
 
-                            </LocalizationProvider>
-                            <br />
-                            <FormControlLabel
-                                label="Paid"
-                                control={
-                                    <Checkbox
-                                        checked={checked}
-                                        onChange={handleChange}
-                                        inputProps={{ 'aria-label': 'controlled' }}
-                                    />} />
-                            <br />
+                                    </LocalizationProvider>
+                                </Box>
+                            ))}
+                            <TextField id="Keys" label="Keys" size="small" />
+                            <TextField id="Description" label="Description" multiline rows={3} size="small" />
+                            <Box sx={{ width: "100%", ml: 2 }}>
+                                <FormControlLabel
+                                    label="Paid"
+                                    control={
+                                        <Checkbox
+                                            checked={checked}
+                                            onChange={handleChange}
+                                            inputProps={{ 'aria-label': 'controlled' }}
+                                        />} />
+                            </Box>
                             {checked && <TextField id="Price" label="Price" InputProps={{
                                 startAdornment: <InputAdornment position="start">INR</InputAdornment>,
                             }} />}
                         </Box>
                         <Button variant='contained'>Create</Button>
-                    </Box>
-                </Card>
+                    </Card>
+                </Box>
             </Box>
         </>
     )
