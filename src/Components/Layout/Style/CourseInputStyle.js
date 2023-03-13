5fc55179e0bec60b5fc5b8b3f4faf10dce84d017
Reshab <naskarreshab2003@gmail.com>
2023-03-13 16:12:52 +0530

    ini

--- src/Components/Layout/Style/CourseInputStyle.js
+++ src/Components/Layout/Style/CourseInputStyle.js
@@ -1,43 +1,56 @@
 const styles = {
     mainContainer: {
-        backgroundSize: 'cover',
-        backgroundImage: "url('https://img.freepik.com/free-photo/blue-abstract-gradient-wave-wallpaper_53876-108364.jpg?w=1380&t=st=1677220877~exp=1677221477~hmac=eef7e55022d564946d00ccac883ea670b3eba52e00e6db06f520b9126d9a4546')",
-        backgroundRepeat: "no-repeat",
+        // backgroundSize: 'cover',
+        // backgroundImage: "url('https://img.freepik.com/free-photo/blue-abstract-gradient-wave-wallpaper_53876-108364.jpg?w=1380&t=st=1677220877~exp=1677221477~hmac=eef7e55022d564946d00ccac883ea670b3eba52e00e6db06f520b9126d9a4546')",
+        // backgroundRepeat: "no-repeat",
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
         justifyContent: "center",
-        width: "100%",
-        height: "100vh",
-        p:2
-    },
-    Card2: {
-        p: 2,
-        display: "flex",
-        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
-        minHeight: "500px",
-        width: "30%",
-    },
-    Box1: {
-        p: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
         width: "100%",
         height: "100%",
-        textAlign: "center"
+        backgroundColor: 'hsl(0deg 0% 96%)',
+        backgroundSize: 'cover',
+    },
+    Box2: {
+        display: "flex",
+        justifyContent: "space-Between",
+        width: "70%",
+        marginTop: "150px",
+        marginBottom:"80px",
+        backgroundColor: 'transparent',
+
     },
     Text1: {
-        p: 1,
+        p: 2,
         fontSize: "35px",
-        fontWeight: "bold"
+        fontWeight: "bold",
     },
-    Text2: {
-        p: 1,
-        fontSize: "20px",
-        fontWeight: "bold"
+    Textfield1: {
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-Between"
     },
+    Box50R: {
+        p: 2,
+        display: "flex",
+        flexDirection: "column",
+        width: "47%",
+        height: "100%",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        borderRadius: "15px"
+    },
+    Box50L: {
+        display:"flex",
+        alignItems:"center",
+        width: "100%",
+        height: "100%",
+        backgroundColor: 'transparent'
+    },
+    Boximg:{
+        width: "45%", 
+        height: "700px"
+    }
 
 };
 export default styles;
