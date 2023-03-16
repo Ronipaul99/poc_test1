cbe4a79b1bfa685f212edda7422007b9d31d82ce
Reshab <naskarreshab2003@gmail.com>
2023-03-16 15:59:29 +0530

    inini

--- src/Components/Layout/Style/ScheduleStyle.js
+++ src/Components/Layout/Style/ScheduleStyle.js
@@ -2,9 +2,9 @@ const styles = {
     mainContainer: {
         display: "flex",
         justifyContent: "center",
-        height: "100vh",
+        height: "100%",
         backgroundColor: 'hsl(0deg 0% 96%)',
-        marginLeft:"200px"
+        marginLeft: "200px"
     },
     sub: {
         width: "95%",
@@ -14,7 +14,8 @@ const styles = {
     calHead: {
         display: "flex",
         width: "100%",
-        height:"50px"
+        height: "50px",
+        marginTop:"20px"
     },
     l50: {
         display: "flex",
@@ -24,7 +25,8 @@ const styles = {
     },
     Text1: {
         fontSize: "25px",
-        fontWeight: "bold"
+        fontWeight: "bold",
+        p: 1
     },
     R50: {
         display: "flex",
@@ -32,8 +34,39 @@ const styles = {
         width: "50%"
     },
     Btn: {
-        height:"30px",
+        height: "30px",
         width: "215px"
-    }
-}
+    },
+    mainB: {
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%"
+    },
+    L502: {
+        width: "27%",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 1
+    },
+    LB1: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    LB2: {
+        height: "auto",
+        display: "flex",
+        justifyContent: "center",
+    },
+    chkBox: {
+        p: 3
+    },
+    R502: {
+        width: "68%",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 1,
+        height: "100%"
+    },
+};
+
 export default styles;
