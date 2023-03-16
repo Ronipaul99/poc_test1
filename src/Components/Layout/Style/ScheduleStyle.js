9921b7ee6e9074f15106981b132cb00b1dd20d7e
Reshab <naskarreshab2003@gmail.com>
2023-03-16 16:02:48 +0530

    addition

--- src/Components/Layout/Style/ScheduleStyle.js
+++ src/Components/Layout/Style/ScheduleStyle.js
@@ -1,16 +1,72 @@
 const styles = {
     mainContainer: {
-        backgroundColor: "#D3D3D3",
+        display: "flex",
+        justifyContent: "center",
+        height: "100%",
+        backgroundColor: 'hsl(0deg 0% 96%)',
+        marginLeft: "200px"
+    },
+    sub: {
+        width: "95%",
+        marginTop: "90px",
+        marginBottom: "80px"
+    },
+    calHead: {
+        display: "flex",
+        width: "100%",
+        height: "50px",
+        marginTop:"20px"
+    },
+    l50: {
+        display: "flex",
+        justifyContent: "flex-start",
+        alignContent: "center",
+        width: "50%"
+    },
+    Text1: {
+        fontSize: "25px",
+        fontWeight: "bold",
+        p: 1
+    },
+    R50: {
+        display: "flex",
+        justifyContent: "flex-end",
+        width: "50%"
+    },
+    Btn: {
+        height: "30px",
+        width: "215px"
+    },
+    mainB: {
+        display: "flex",
+        justifyContent: "space-between",
+        width: "100%"
+    },
+    L502: {
+        width: "27%",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 1
+    },
+    LB1: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    LB2: {
+        height: "auto",
+        display: "flex",
+        justifyContent: "center",
+    },
+    chkBox: {
+        p: 3
+    },
+    R502: {
+        width: "68%",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 1,
         height: "100%"
     },
-    Card: {
-        backgroundSize: 'cover',
-        backgroundImage: "url('https://img.freepik.com/free-photo/blue-abstract-gradient-wave-wallpaper_53876-108364.jpg?w=1380&t=st=1677220877~exp=1677221477~hmac=eef7e55022d564946d00ccac883ea670b3eba52e00e6db06f520b9126d9a4546')",
-        backgroundRepeat: "no-repeat",
-        display: "flex",
-        flexDirection: "column",
-        width: "100%",
-        height: "100vh"
-    },
-}
+};
+
 export default styles;
