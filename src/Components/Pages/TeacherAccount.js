51527503093713bcfe530ac0963532fe41a0bc0d
Reshab <naskarreshab2003@gmail.com>
2023-03-20 14:26:14 +0530

    uno

--- src/Components/Pages/TeacherAccount.js
+++ src/Components/Pages/TeacherAccount.js
@@ -20,16 +20,16 @@ const TeacherAccount = () => {
     return (
         <Box sx={styles.mainContainer}>
             <Box sx={styles.Box}>
-                <Box sx={{ width: "100%", display: "flex" }}>
-                    <Box sx={{ width: "70%" }}>
+                <Box sx={styles.one}>
+                    <Box sx={styles.Two}>
                         <Box sx={styles.Box1}>
-                            <Box sx={{ height: "10%", width: "100%" }}>
-                                <Box sx={{ width: "100%" }}>
-                                    <Box sx={{ width: "100%", height: "100%", display: "flex" }}>
-                                        <Box sx={{ width: "70%", alignItems: "center" }}>
+                            <Box sx={styles.Three}>
+                                <Box sx={styles.Four}>
+                                    <Box sx={styles.Five}>
+                                        <Box sx={styles.Six}>
                                             <Typography sx={styles.Text1}>Account Preview</Typography>
                                         </Box>
-                                        <Box sx={{ width: "30%", display: "flex", justifyContent: "flex-end", alignItems: "center" }}>
+                                        <Box sx={styles.Seven}>
                                             <TextField
                                                 id="outlined-select-currency"
                                                 select
@@ -48,37 +48,40 @@ const TeacherAccount = () => {
                             </Box>
                             <Box sx={styles.Box12}>
                                 <Box sx={styles.U50}>
-                                    <Card sx={{ height: "100%", width: "100%",boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"}}>
-
+                                    <Card sx={styles.Eight}>
+                                        {/* upper box */}
 
                                     </Card>
                                 </Box>
                                 <Box sx={styles.L50}>
-                                    <Box sx={{ height: "100%", width: "100%", display: "flex", justifyContent: "space-between" }}>
-                                        <Card sx={{ width: "28%", height: "100%" ,backgroundColor: "#aff3eb", opacity: "inherit", boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)" }}>
+                                    <Box sx={styles.Nine}>
+                                        <Card sx={styles.Ten}>
+                                            {/* Card1 */}
 
                                         </Card>
-                                        <Card sx={{ width: "28%", height: "100%",backgroundColor: "#dbcadd", opacity: "inherit", boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"  }}>
+                                        <Card sx={styles.Eleven}>
+
+                                            {/* card2 */}
                                         </Card>
-                                        <Card sx={{ width: "28%", height: "100%",backgroundColor: "#dbcadd", opacity: "inherit", boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"  }}>
+                                        <Card sx={styles.Twelve}>
+                                            {/* Card3 */}
 
                                         </Card>
                                     </Box>
-
                                 </Box>
                             </Box>
                         </Box>
                     </Box>
-                    <Box sx={{ width: "30%", height: "100%", display: "flex" }}>
-                        <Box sx={{ height: "100%", width: "100%", display: "flex", flexDirection: "column", justifyContent: "space-between", alignItems: "center" }}>
-                            <Card sx={{ width: "80%", height: "27%", backgroundColor: "#2a2a2c", boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)" }}>
-
+                    <Box sx={styles.Thirteen}>
+                        <Box sx={styles.Fourteen}>
+                            <Card sx={styles.Fifteen}>
+                                {/* right side top card */}
                             </Card>
-                            <Card sx={{ width: "80%", height: "40%", backgroundColor: "#aff3eb", opacity: "inherit", boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)" }}>
-
+                            <Card sx={styles.Sixteen}>
+                                {/* right side middle card */}
                             </Card>
-                            <Card sx={{ width: "80%", height: "27%", backgroundColor: "#2a2a2c", boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)" }}>
-
+                            <Card sx={styles.Seventeen}>
+                                {/* right side bottom card */}
                             </Card>
                         </Box>
                     </Box>
--- src/Components/Style/TeacherAccountStyle.js
+++ src/Components/Style/TeacherAccountStyle.js
@@ -3,8 +3,8 @@ const styles = {
         display: "flex",
         justifyContent: "center",
         height: "80vh",
-        marginTop:"50px",
-        marginBottom:"50px"
+        marginTop: "50px",
+        marginBottom: "50px"
     },
     Box1: {
 
@@ -15,27 +15,120 @@ const styles = {
         width: "100%",
         height: "90%",
         display: "flex",
-        flexDirection:"column" 
+        flexDirection: "column"
     },
     U50: {
         width: "100%",
-        height:"80%"
+        height: "80%"
     },
     L50: {
         width: "100%",
-        height:"20%",
-        marginTop:"20px"
+        height: "20%",
+        marginTop: "20px"
     },
     Text1: {
         fontSize: "25px",
         fontWeigth: "bold",
         color: "grey",
-        p:2
+        p: 2
     },
-    Box:{
-         width: "80%", 
-         height: "100%", 
-         display: "flex" 
+    Box: {
+        width: "80%",
+        height: "100%",
+        display: "flex"
+    },
+    one: {
+        width: "100%",
+        display: "flex"
+    },
+    Two: {
+        width: "70%"
+    },
+    Three: {
+        height: "10%",
+        width: "100%"
+    },
+    Four: {
+        width: "100%"
+    },
+    Five: {
+        width: "100%",
+        height: "100%",
+        display: "flex"
+    },
+    Six: {
+        width: "70%",
+        alignItems: "center"
+    },
+    Seven: {
+        width: "30%",
+        display: "flex",
+        justifyContent: "flex-end",
+        alignItems: "center"
+    },
+    Eight: {
+        height: "100%",
+        width: "100%",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+    },
+    Nine: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    Ten: {
+        width: "28%",
+        height: "100%",
+        backgroundColor: "#aff3eb",
+        opacity: "inherit",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+    },
+    Eleven: {
+        width: "28%",
+        height: "100%",
+        backgroundColor: "#dbcadd",
+        opacity: "inherit",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+    },
+    Twelve: {
+        width: "28%",
+        height: "100%",
+        backgroundColor: "#dbcadd",
+        opacity: "inherit",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+    },
+    Thirteen: {
+        width: "30%",
+        height: "100%",
+        display: "flex"
+    },
+    Fourteen: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    Fifteen: {
+        width: "80%",
+        height: "27%",
+        backgroundColor: "#2a2a2c",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+    },
+    Sixteen: {
+        width: "80%",
+        height: "40%",
+        backgroundColor: "#aff3eb",
+        opacity: "inherit",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
+    },
+    Seventeen: {
+        width: "80%",
+        height: "27%",
+        backgroundColor: "#2a2a2c",
+        boxShadow: "0 15px 30px rgba(0, 0, 0, 0.1)"
     }
 };
 export default styles;
