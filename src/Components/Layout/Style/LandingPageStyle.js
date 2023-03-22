05de6e39d51a848c104a1ddc9419e3bcb34bcd46
Reshab <naskarreshab2003@gmail.com>
2023-03-22 18:00:00 +0530

    ioi

---
+++ src/Components/Layout/Style/LandingPageStyle.js
@@ -0,0 +1,421 @@
+const styles = {
+    mainContainer: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    FirstCard: {
+        height: "78.5vh",
+        backgroundSize: 'cover',
+        backgroundImage: "url('https://static.uacdn.net/production/_next/static/images/home/hero_banner_landing_new.svg')",
+        backgroundRepeat: "no-repeat",
+    },
+    One: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        height: "80%",
+        alignItems: 'center'
+    },
+    Two: {
+        height: "80%",
+        display: "flex",
+        alignItems: "center", p: 1,
+        justifyContent: "center",
+        width: "85%"
+    },
+    Three: {
+        width: "100%",
+        height: "100%"
+    },
+    Four: {
+        width: "40%",
+        display: "flex",
+        alignItems: "flex-end",
+        height: "100%"
+    },
+    Five: {
+        fontSize: "56px",
+        fontWeight: "700",
+        fontFamily: "AvertaStd, -apple-system, BlinkMacSystemFont, sans-serif !important",
+        color: "rgb(60, 72, 82)"
+    },
+    Six: {
+        height: "20%",
+        width: "85%"
+    },
+    Seven: {
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        p: 1,
+        justifyContent: "flex-start"
+    },
+    Btn: {
+        p: 2.5
+    },
+    Text1: {
+        fontSize: "20px",
+        fontWeight: "700",
+        color: "white"
+    },
+    Eight: {
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        height: "20%",
+        alignItems: "center"
+    },
+    Nine: {
+        height: "80%",
+        display: "flex",
+        alignItems: "center",
+        p: 1,
+        justifyContent: "flex-start",
+        width: "85%"
+    },
+    Ten: {
+        display: "flex"
+    },
+    Text2: {
+        fontSize: "17px",
+        fontWeight: "100"
+    },
+    Text3: {
+        color: "#08BD80",
+        fontWeight: "100",
+        fontSize: "17px",
+        ml: 2
+    },
+    Eleven: {
+        height: "800px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    Twelve: {
+        width: "70%",
+        height: "50vh",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 4,
+        borderRadius: "11px"
+    },
+    Thirteen: {
+        height: "100%",
+        width: "100%",
+        display: "flex"
+    },
+    Forteen: {
+        height: "100%",
+        width: "40%"
+    },
+    Fifteen: {
+        height: "100%",
+        width: "60%"
+    },
+    Sixteen: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        flexDirection: "column"
+    },
+    Seventeen: {
+        width: "100%",
+        height: "85%",
+        ml: 4
+    },
+    Eighteen: {
+        width: "70%"
+    },
+    Text4: {
+        fontSize: "14px",
+        fontWeight: "700",
+        color: "rgb(45, 129, 247)"
+    },
+    Text5: {
+        fontSize: "32px",
+        fontWeight: "700",
+        color: "rgb(60, 72, 82)",
+        marginTop: "10px"
+    },
+    Text6: {
+        fontWeight: "400",
+        color: "#7A8B94",
+        fontSize: "20px",
+        marginTop: "10px"
+    },
+    Twenty: {
+        display: "flex",
+        flexDirection: "column",
+        marginTop: "15px"
+    },
+    Text7: {
+        fontSize: "16px",
+        fontWeight: "400",
+        color: "rgb(60, 72, 82)"
+    },
+    Text8: {
+        marginTop: "8px",
+        fontSize: "16px",
+        fontWeight: "400",
+        color: "rgb(60, 72, 82)"
+    },
+    Twentyone: {
+        width: "100%",
+        height: "15%",
+        display: "flex",
+        justifyContent: "flex-start",
+        ml: 4,
+        alignItems: "flex-end"
+    },
+    Btn1: {
+        width: "160px",
+        height: "40px",
+        backgroundColor: "rgb(60, 72, 82)"
+    },
+    Icon: {
+        mr: 1
+    },
+    Eleven1: {
+        height: "500px",
+        display: "flex",
+        justifyContent: "center"
+    },
+    B1: {
+        width: "70%",
+        height: "40vh"
+    },
+    B2: {
+        height: "100%",
+        width: "100%",
+        display: "flex",
+        justifyContent: "space-between"
+    },
+    B3: {
+        width: "30%",
+        height: "100%"
+    },
+    B4: {
+        height: "100%",
+        width: "100%"
+    },
+    B5: {
+        height: "60%",
+        width: "100%"
+    },
+    B6: {
+        height: "40%",
+        width: "100%",
+        p: 1
+    },
+    Text9: {
+        fontSize: "22px",
+        fontWeight: "bold",
+        color: "#3C4852",
+        marginTop: "10px"
+    },
+    Text10: {
+        fontSize: "16px",
+        fontWeight: "400",
+        color: "#3C4852",
+        marginTop: "10px"
+    },
+    B7: {
+        width: "30%",
+        height: "100%"
+    },
+    B8: {
+        height: "100%",
+        width: "100%"
+    },
+    B9: {
+        height: "60%",
+        width: "100%"
+    },
+    B10: {
+        height: "40%",
+        width: "100%",
+        p: 1
+    },
+    Text11: {
+        fontSize: "22px",
+        fontWeight: "bold",
+        color: "#3C4852",
+        marginTop: "10px"
+    },
+    Text12: {
+        fontSize: "16px",
+        fontWeight: "400",
+        color: "#3C4852",
+        marginTop: "10px"
+    },
+    B11: {
+        width: "100%",
+        height: "100%",
+        display: "flex"
+    },
+    B12: {
+        width: "50%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    B13: {
+        width: "62%",
+        height: "80%"
+    },
+    Text13: {
+        fontSize: "40px",
+        fontWeight: "700",
+        color: "#3C4852"
+    },
+    Text14: {
+        fontSize: "20px",
+        color: "#3C4852",
+        marginTop: "10px"
+    },
+    B14: {
+        display: "flex",
+        justifyContent: "space-between",
+        height: "50px",
+        width: "100%",
+        marginTop: "20px"
+    },
+    B15: {
+        width: "45%",
+        height: "100%"
+    },
+    B16: {
+        width: "50%",
+        height: "100%"
+    },
+    B17: {
+        height: "100%",
+        width: "100%"
+    },
+    B18: {
+        height: "1050px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    B19: {
+        width: "70%",
+        height: "690px",
+        p: 4
+    },
+    B20: {
+        display: "flex",
+        height: "100%",
+        width: "100%"
+    },
+    B21: {
+        width: "50%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "flex-start"
+    },
+    B22: {
+        width: "65%"
+    },
+    Text15: {
+        fontSize: "40px",
+        fontWeight: "bold",
+        color: "#3C4852"
+    },
+    Text16: {
+        fontSize: "20px",
+        color: "#3C4852",
+        marginTop: "16px"
+    },
+    Btn2: {
+        marginTop: "20px",
+        p: 2
+    },
+    B23: {
+        width: "50%",
+        height: "100%"
+    },
+    B24: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+    B25: {
+        width: "47%",
+        height: "100%"
+    },
+    B26: {
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between"
+    },
+    B27: {
+        height: "30%",
+        width: "100%",
+        backgroundSize: 'cover',
+        backgroundImage: "url('https://static.uacdn.net/web-cms/highlightgoals_96d24a04e7_a8e8de225d.svg?q=75&auto=format%2Ccompress&w=384')",
+        backgroundRepeat: "no-repeat",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 3
+    },
+    B271: {
+        height: "30%",
+        width: "100%",
+        backgroundSize: 'cover',
+        backgroundImage: "url('https://static.uacdn.net/web-cms/highlightlive_b05ed1f073_a8042bd7a8.svg?q=75&auto=format%2Ccompress&w=384')",
+        backgroundRepeat: "no-repeat",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 3
+    },
+    B272: {
+        height: "30%",
+        width: "100%",
+        backgroundSize: 'cover',
+        backgroundImage: "url('https://static.uacdn.net/web-cms/highlightminswatched_fe52bc4026_ca82eba8b0.svg?q=75&auto=format%2Ccompress&w=384')",
+        backgroundRepeat: "no-repeat",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 3
+    },
+    B28: {
+        width: "47%",
+        height: "60%"
+    },
+    B29: {
+        height: "45%",
+        width: "100%",
+        backgroundSize: 'cover',
+        backgroundImage: "url('https://static.uacdn.net/web-cms/hightlighteducators_c894789a76_58c2a07e52.svg?q=75&auto=format%2Ccompress&w=640')",
+        backgroundRepeat: "no-repeat",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 3
+    },
+    B291: {
+        height: "45%",
+        width: "100%",
+        backgroundSize: 'cover',
+        backgroundImage: "url('https://static.uacdn.net/web-cms/highlightfreecourses_bb697dbd44_47fe832f07.svg?q=75&auto=format%2Ccompress&w=640')",
+        backgroundRepeat: "no-repeat",
+        boxShadow: "0 35px 100px rgba(0, 0, 0, 0.1)",
+        p: 3
+    },
+    Text17: {
+        fontSize: "20px",
+        fontWeight: "bold",
+        color: "#3C4852"
+    },
+    Text18: {
+        fontSize: "40px",
+        fontWeight: "bold",
+        color: "#3C4852"
+    }
+};
+export default styles;
--- src/Components/Pages/Landing.js
+++ src/Components/Pages/Landing.js
@@ -1,11 +1,246 @@
+import { Box, Button, Card, Typography } from '@mui/material'
 import React from 'react'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import styles from '../Layout/Style/LandingPageStyle'
 
 function Landing() {
   return (
-    <div className='container'>
+    <Box sx={styles.mainContainer}>
+      {/* Upper part */}
+      <Box sx={styles.FirstCard}>
+        <Box sx={styles.One}>
+          <Box sx={styles.Two}>
+            <Box sx={styles.Three}>
+              <Box sx={styles.Four}>
+                <Typography sx={styles.Five}>India’s largest learning platform</Typography>
+              </Box>
+            </Box>
+          </Box>
+          <Box sx={styles.Six}>
+            <Box sx={styles.Seven}>
+              <Button variant='contained' color='success' sx={styles.Btn}><Typography sx={styles.Text1}>start learning</Typography></Button>
+            </Box>
+          </Box>
+        </Box>
+        <Box sx={styles.Eight}>
+          <Box sx={styles.Nine}>
+            <Box sx={styles.Ten}>
+              <Typography sx={styles.Text2}>Popular goals</Typography>
+              <Typography sx={styles.Text3}>UPSC CSE - GS</Typography>
+              <Typography sx={styles.Text3}>IIT JEE</Typography>
+              <Typography sx={styles.Text3}>NEET UG</Typography>
+              <Typography sx={styles.Text3}>CAT & Other MBA Ent...</Typography>
+              <Typography sx={styles.Text3}>Book Exams</Typography>
+              <Typography sx={styles.Text3}>CA Intermediate</Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+      {/* card number 1 */}
+      <Box sx={styles.Eleven}>
+        <Card sx={styles.Twelve}>
+          <Box sx={styles.Thirteen}>
+            <Box sx={styles.Forteen}>
+              <img style={{
+                boxSizing: "border-box",
+                objectFit: "cover",
+                height: "100%",
+                width: "100%"
+              }} src="	https://static.uacdn.net/web-cms/stores2_Batch_22396665d0.png?q=75&auto=format%2Ccompress&w=1920" alt="pic here" />
+            </Box>
+            <Box sx={styles.Fifteen}>
+              <Box sx={styles.Sixteen}>
+                <Box sx={styles.Seventeen}>
+                  <Box sx={styles.Eighteen}>
+                    <Typography sx={styles.Text4}>INTRODUCING</Typography>
+                    <Typography sx={styles.Text5}>Unacademy Centres for IIT JEE and NEET UG</Typography>
+                    <Typography sx={styles.Text6}>Admissions open in Kota, Delhi, Chandigarh, Sikar, Indore and 20+ other cities!</Typography>
+                    <Box sx={styles.Twenty}>
+                      <Typography sx={styles.Text7}> <CheckCircleOutlineIcon fontSize='small' sx={styles.Icon} />Learn from top educators in your city</Typography>
+                      <Typography sx={styles.Text8}> <CheckCircleOutlineIcon fontSize='small' sx={styles.Icon} />In-person classes & doubt solving</Typography>
+                      <Typography sx={styles.Text8}> <CheckCircleOutlineIcon fontSize='small' sx={styles.Icon} />Bonus access to online learning</Typography>
+                    </Box>
+                  </Box>
+                </Box>
+                <Box sx={styles.Twentyone}>
+                  <Button variant='contained' sx={styles.Btn1}>Find center</Button>
+                </Box>
+              </Box>
+
+            </Box>
+          </Box>
+        </Card>
+      </Box>
+      {/* Box number 2 */}
+      <Box sx={styles.Eleven1}>
+        <Box sx={styles.B1}>
+          <Box sx={styles.B2}>
+            {/* SubCard 1 */}
+            <Box sx={styles.B3}>
+              <Box sx={styles.B4}>
+                <Box sx={styles.B5}>
+                  <img style={{
+                    boxSizing: "border-box",
+                    objectFit: "cover",
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "11px"
+                  }} src="https://static.uacdn.net/web-cms/dailyliveclasses_0c0023950f_c187155ee3.svg?q=75&auto=format%2Ccompress&w=384" alt="pic here" />
+                </Box>
+                <Box sx={styles.B6}>
+                  <Typography sx={styles.Text9}>Daily live classes</Typography>
+                  <Typography sx={styles.Text10}>Chat with educators, ask questions, answer live polls, and get your doubts cleared - all while the class is going on</Typography>
+                </Box>
+              </Box>
+            </Box>
+
+            {/* SubCard 2 */}
+            <Box sx={styles.B7}>
+              <Box sx={styles.B8}>
+                <Box sx={styles.B9}>
+                  <img style={{
+                    boxSizing: "border-box",
+                    objectFit: "cover",
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "11px"
+                  }} src="https://static.uacdn.net/web-cms/syllabus_a9da21d824_b20b285483.svg?q=75&auto=format%2Ccompress&w=384" alt="pic here" />
+                </Box>
+                <Box sx={styles.B10}>
+                  <Typography sx={styles.Text11}>Practice and revise</Typography>
+                  <Typography sx={styles.Text12}>Learning isn't just limited to classes with our practice section, mock tests and lecture notes shared as PDFs for your revision</Typography>
+                </Box>
+              </Box>
+            </Box>
+
+            {/* SubCard 3 */}
+            <Box sx={styles.B7}>
+              <Box sx={styles.B8}>
+                <Box sx={styles.B9}>
+                  <img style={{
+                    boxSizing: "border-box",
+                    objectFit: "cover",
+                    height: "100%",
+                    width: "100%",
+                    borderRadius: "11px"
+                  }} src="https://static.uacdn.net/web-cms/learnanytimeanywhere_cb19d5de30_b92bc0c6a1.svg?q=75&auto=format%2Ccompress&w=384" alt="pic here" />
+                </Box>
+                <Box sx={styles.B10}>
+                  <Typography sx={styles.Text11}>Learn anytime, anywhere</Typography>
+                  <Typography sx={styles.Text12}>One subscription gets you access to all our live and recorded classes to watch from the comfort of any of your devices</Typography>
+                </Box>
+              </Box>
+            </Box>
+
+          </Box>
+
+        </Box>
+      </Box>
+
+      {/* Card number 2 */}
+      <Box sx={styles.Eleven1}>
+        <Card sx={styles.Twelve}>
+          <Box sx={styles.B11}>
+            <Box sx={styles.B12}>
+              <Box sx={styles.B13}>
+                <Typography sx={styles.Text13}>Get the learning app</Typography>
+                <Typography sx={styles.Text14}>Download lessons and learn anytime, anywhere with the Unacademy app</Typography>
+                <Box sx={styles.B14}>
+                  <Box sx={styles.B15}>
+                    <img style={{
+                      boxSizing: "border-box",
+                      objectFit: "cover",
+                      height: "100%",
+                      width: "100%",
+                      borderRadius: "11px"
+                    }} src="https://static.uacdn.net/production/_next/static/images/play_store.png?q=75&auto=format%2Ccompress&w=128" alt="pic here" />
+                  </Box>
+                  <Box sx={styles.B15}>
+                    <img style={{
+                      boxSizing: "border-box",
+                      objectFit: "cover",
+                      height: "100%",
+                      width: "100%",
+                      borderRadius: "11px"
+                    }} src="https://static.uacdn.net/production/_next/static/images/play_store.png?q=75&auto=format%2Ccompress&w=128" alt="pic here" />
+                  </Box>
+                </Box>
+              </Box>
+
+            </Box>
+            <Box sx={styles.B16}>
+              <Box sx={styles.B17}>
+                <img style={{
+                  boxSizing: "border-box",
+                  objectFit: "cover",
+                  height: "404px",
+                  width: "381px"
+                }} src="https://static.uacdn.net/production/_next/static/images/newApp.png?q=75&auto=format%2Ccompress&w=640" alt="pic here" />
+              </Box>
+            </Box>
+          </Box>
+        </Card>
+      </Box>
 
 
-    </div>
+      {/* Card number 3 */}
+
+      <Box sx={styles.B18}>
+        <Box sx={styles.B19}>
+          <Box sx={styles.B20}>
+            <Box sx={styles.B21}>
+              <Box sx={styles.B22}>
+                <Typography sx={styles.Text15}>Start learning with Uducate</Typography>
+                <Typography sx={styles.Text16}>Get unlimited access to structured courses & doubt clearing sessions</Typography>
+                <Button variant='contained' sx={styles.Btn2}>Start learning</Button>
+              </Box>
+            </Box>
+            <Box sx={styles.B23}>
+              <Box sx={styles.B24}>
+                <Box sx={styles.B25}>
+
+                  <Box sx={styles.B26}>
+                    {/* inner left card 1 */}
+                    <Card sx={styles.B27}>
+                      <Typography sx={styles.Text17}>Exam categories</Typography>
+                      <Typography sx={styles.Text18}>60+</Typography>
+                    </Card>
+                    {/* inner left card 2 */}
+                    <Card sx={styles.B271}>
+                      <Typography sx={styles.Text17}>Daily live classes</Typography>
+                      <Typography sx={styles.Text18}>1.5k+</Typography>
+                    </Card>
+                    {/* inner left card 3 */}
+                    <Card sx={styles.B272}>
+                      <Typography sx={styles.Text17}>Mins watched</Typography>
+                      <Typography sx={styles.Text18}>3.2B+</Typography>
+                    </Card>
+                  </Box>
+                </Box>
+                <Box sx={styles.B28}>
+                  <Box sx={styles.B26}>
+                    {/* inner right card 1 */}
+                    <Card sx={styles.B29}>
+                      <Typography sx={styles.Text17}>Educator</Typography>
+                      <Typography sx={styles.Text18}>40k+</Typography>
+                    </Card>
+                    {/* inner right card 2 */}
+                    <Card sx={styles.B291}>
+                      <Typography sx={styles.Text17}>Video lessons</Typography>
+                      <Typography sx={styles.Text18}>1M+</Typography>
+                    </Card>
+                  </Box>
+
+                </Box>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+
+
+    </Box >
   )
 }
 
